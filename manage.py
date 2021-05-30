97f440c4fa10b4e113956c3f780695a993620482
Kathoey <nicholasnguyen75@gmail.com>
2021-05-30 01:49:42 -0400

    nc

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WatchSesh.core.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WatchSesh.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
