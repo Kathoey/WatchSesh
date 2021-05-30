3bf9def260b47879b6f329fa2c2c41a311609975
Kathoey <nicholasnguyen75@gmail.com>
2021-05-30 01:38:17 -0400

    nc

--- WatchSesh/asgi.py
+++ WatchSesh/asgi.py
@@ -16,7 +16,7 @@ import chat.routing
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WatchSesh.settings")
 
 application = ProtocolTypeRouter({
-  "https": get_asgi_application(),
+  "http": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
