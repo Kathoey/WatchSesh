55e3849fbbfe2c875eb11aff14be1bb733b8be34
Kathoey <nicholasnguyen75@gmail.com>
2021-07-11 02:18:38 -0400

    url

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,9 @@
 # chat/urls.py
-from django.conf.urls import url
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.lobby, name='lobby'),
-    url('<str:room_name>/', views.room, name='room'),
+    path('<str:room_name>/', views.room, name='room'),
 ]
