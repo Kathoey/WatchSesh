d462aa56b86689140a9dd5bed6208c35001d34b0
Kathoey <nicholasnguyen75@gmail.com>
2021-07-11 03:40:40 -0400

    Update asgi.py

--- WatchSesh/asgi.py
+++ WatchSesh/asgi.py
@@ -1,32 +1,5 @@
-"""
-ASGI config for WatchSesh project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
 '''
-import os
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WatchSesh.settings")
-application = get_asgi_application()
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import chat.routing
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
-'''
-
 import os
 
 from django.conf.urls import url
@@ -54,3 +27,11 @@ application = ProtocolTypeRouter({
         ])
     ),
 })
+'''
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+application = get_default_application()
