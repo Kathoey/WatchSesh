f9465ac07acffcd95e3ec80bec65a26b88584f2e
Kathoey <nicholasnguyen75@gmail.com>
2021-07-11 03:51:34 -0400

    Template and consumer changes

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,37 +1,38 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
 
 #TODO: fix sending of data to include all the stuff and types given
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, close_code):
+    def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         message_type = text_data_json['type']
         user = text_data_json['user']
         # Send message to room group
-        await self.channel_layer.group_send(
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': message_type,
@@ -41,12 +42,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     # Receive message from room group
-    async def chat_message(self, event):
+    def chat_message(self, event):
         message = event['message']
         message_type = event['type']
         user = event['user']
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
+        self.send(text_data=json.dumps({
             'message': message,
             'type': message_type,
             'user': user,
@@ -54,12 +55,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         }))
 
     # Receive video from room group
-    async def video_load(self, event):
+    def video_load(self, event):
         message = event['message']
         message_type = event['type']
         user = event['user']
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
+        self.send(text_data=json.dumps({
             'message': message,
             'type': message_type,
             'user': user,
@@ -67,72 +68,72 @@ class ChatConsumer(AsyncWebsocketConsumer):
         }))
 
     # On room join
-    async def room_join(self, event):
+    def room_join(self, event):
         message = event['message']
         message_type = event['type']
         user = event['user']
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
+        self.send(text_data=json.dumps({
             'message': message,
             'type': message_type,
             'user': user,
 
         }))
 
-    async def pause_message(self, event):
+    def pause_message(self, event):
         message = event['message']
         message_type = event['type']
         user = event['user']
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
+        self.send(text_data=json.dumps({
             'message': message,
             'type': message_type,
             'user': user,
 
         }))
 
-    async def unpause_message(self, event):
+    def unpause_message(self, event):
         message = event['message']
         message_type = event['type']
         user = event['user']
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
+        self.send(text_data=json.dumps({
             'message': message,
             'type': message_type,
             'user': user,
 
         }))
 
-    async def seek_message(self, event):
+    def seek_message(self, event):
         message = event['message']
         message_type = event['type']
         user = event['user']
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
+        self.send(text_data=json.dumps({
             'message': message,
             'type': message_type,
             'user': user,
 
         }))
 
-    async def room_join(self, event):
+    def room_join(self, event):
         message = event['message']
         message_type = event['type']
         user = event['user']
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
+        self.send(text_data=json.dumps({
             'message': message,
             'type': message_type,
             'user': user,
 
         }))
 
-    async def room_leave(self, event):
+    def room_leave(self, event):
         message = event['message']
         message_type = event['type']
         user = event['user']
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
+        self.send(text_data=json.dumps({
             'message': message,
             'type': message_type,
             'user': user,
