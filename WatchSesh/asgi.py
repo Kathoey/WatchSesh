c7f478fc3e07d46d1f48558b292daf2a0f177dc1
Kathoey <nicholasnguyen75@gmail.com>
2021-07-10 22:11:09 -0400

    Added daphne support

--- WatchSesh/asgi.py
+++ WatchSesh/asgi.py
@@ -7,14 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 import os
-
+import django
+from channels.routing import get_default_application
+'''
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
-
+'''
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WatchSesh.settings")
-
+django.setup()
+application = get_default_application()
+'''
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
@@ -23,4 +27,4 @@ application = ProtocolTypeRouter({
         )
     ),
 })
-
+'''
