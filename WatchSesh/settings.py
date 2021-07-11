12f48ab11598d9cce5022a733312cfa469b817a6
Kathoey <nicholasnguyen75@gmail.com>
2021-07-11 03:43:05 -0400

    Fixed bug

--- WatchSesh/settings.py
+++ WatchSesh/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/chat'
 
 # Channels
-ASGI_APPLICATION = "core.routing.application"
+ASGI_APPLICATION = "WatchSesh.routing.application"
 
 
 CHANNEL_LAYERS = {
